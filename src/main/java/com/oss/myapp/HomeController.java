b760e5f87b40e0eaf6f24af728b593d6b13640a6
ygygood <ygygood@naver.com>
2021-05-26 18:51:41 +0900

    i don't know
    what the hell

--- src/main/java/com/oss/myapp/HomeController.java
+++ src/main/java/com/oss/myapp/HomeController.java
@@ -29,6 +29,8 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
+		logger.info("lint test");
+		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
